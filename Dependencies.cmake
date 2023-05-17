d6ec9c0ff343e6c94abdee1972cfa9a9df95ffe8
jngl <jngl92@gmail.com>
2023-05-17 18:39:02 +0200

    remove CLI11, FTXUI and lefticus/tools

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -28,16 +28,4 @@ function(factrory_craft_setup_dependencies)
     cpmaddpackage("gh:catchorg/Catch2@3.3.2")
   endif()
 
-  if(NOT TARGET CLI11::CLI11)
-    cpmaddpackage("gh:CLIUtils/CLI11@2.3.2")
-  endif()
-
-  if(NOT TARGET ftxui::screen)
-    cpmaddpackage("gh:ArthurSonzogni/FTXUI#e23dbc7473654024852ede60e2121276c5aab660")
-  endif()
-
-  if(NOT TARGET tools::tools)
-    cpmaddpackage("gh:lefticus/tools#update_build_system")
-  endif()
-
 endfunction()
